757ecf32ef6657c084a2eb30a4557501d9046dee
boatgrandad <devops@cloudandtree.xyz>
2025-06-13 10:08:11 +0100

    Update hello_world.sh
    The end of the script

--- hello_world.sh
+++ hello_world.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Hello, world! This is Stephen's test of CI."
-echo "Today is very sunny and there's Pizza in the air.
+echo "Today is very sunny and there's Pizza in the air."
+echo "And that is CI."
